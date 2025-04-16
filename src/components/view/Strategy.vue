e3b1fa0296cd18b07d1abb3a4ec7de9b42b9ae3c
zdplay <3331726@gmail.com>
2025-04-16 08:02:10 +0800

    调整攻略按钮页面

--- src/components/view/Strategy.vue
+++ src/components/view/Strategy.vue
@@ -1,15 +1,53 @@
 <template>
-  <div class="strategy-container">
-    <iframe
-      src="https://docs.qq.com/sheet/DQlNPSHdVVkdxZ0l4?tab=qej3zz"
-      style="width: 100%; height: 100%; border: none;"
-    ></iframe>
-  </div>
-</template>
-
-<style scoped>
-.strategy-container {
-  width: 100%;
-  height: calc(100vh - 64px); /* 调整高度，留出顶部栏的空间 */
-}
-</style>
+    <div class="strategy-container">
+      <div class="spacer"></div>
+  
+      <iframe
+        :src="iframeSrc"
+        style="width: 100%; height: 100%; border: none;"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        iframeSrc: "https://docs.qq.com/sheet/DQlNPSHdVVkdxZ0l4?tab=qej3zz",
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .strategy-container {
+    width: 100%;
+    height: calc(100vh - 64px);
+    overflow: hidden;
+  }
+  
+  .spacer {
+    display: none;
+  }
+  
+  iframe {
+    height: 100%;
+  }
+  
+  @media (max-width: 767px) {
+    .strategy-container {
+      height: calc(100vh - 64px);
+    }
+  
+    .spacer {
+      display: block;
+      width: 100%;
+      height: 48px;
+    }
+  
+    iframe {
+      height: calc(100% - 48px);
+    }
+  }
+  </style>
+  
