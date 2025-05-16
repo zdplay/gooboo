8c402f2ce3687700b893e4a3ea0e99ba96539251
zdplay <3331726@gmail.com>
2025-05-16 16:39:31 +0800

    修改下路径指向

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/gooboo/' : '/'
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || (process.env.NODE_ENV === 'production' ? '/gooboo/' : '/')
 }
