57c7b64e0f5895ce928d74b2d309515907ed50e4
zdplay <3331726@gmail.com>
2025-10-02 09:52:44 +0800

    调整每日签到礼包

--- src/js/modules/consecutiveSignIn/relic.js
+++ src/js/modules/consecutiveSignIn/relic.js
@@ -1,18 +1,18 @@
 export default {
     consecutiveSignIn1: {icon: 'mdi-dots-triangle', color: 'light-blue', effect: [
-        {name: 'currencyMiningScrapGain', type: 'mult', value: 1.1}
+        {name: 'currencyMiningScrapGain', type: 'mult', value: 1.3}
     ]},
     consecutiveSignIn2: {icon: 'mdi-account-group', color: 'green', effect: [
         {name: 'villageWorker', type: 'base', value: 8}
     ]},
     consecutiveSignIn3: {icon: 'mdi-sword', color: 'red', effect: [
-        {name: 'hordeAttack', type: 'mult', value: 1.1}
+        {name: 'hordeAttack', type: 'mult', value: 1.15}
     ]},
     consecutiveSignIn4: {icon: 'mdi-sprout', color: 'orange', effect: [
-        {name: 'farmCropGain', type: 'mult', value: 1.1}
+        {name: 'farmCropGain', type: 'mult', value: 1.15}
     ]},
     consecutiveSignIn5: {icon: 'mdi-palette', color: 'pink', effect: [
-        {name: 'currencyGalleryBeautyGain', type: 'mult', value: 1.1}
+        {name: 'currencyGalleryBeautyGain', type: 'mult', value: 1.15}
     ]},
     consecutiveSignIn6: {icon: 'mdi-hammer-wrench', color: 'purple', effect: [
         {name: 'miningPickaxePremiumCraftingSlots', type: 'base', value: 1}
@@ -21,7 +21,7 @@ export default {
         {name: 'currencyVillageOfferingGain', type: 'mult', value: 1.2}
     ]},
     consecutiveSignIn8: {icon: 'mdi-crystal-ball', color: 'cyan', effect: [
-        {name: 'currencyHordeMysticalShardCap', type: 'mult', value: 1.3}
+        {name: 'currencyHordeMysticalShardCap', type: 'mult', value: 1.2}
     ]},
     consecutiveSignIn9: {icon: 'mdi-school', color: 'teal', effect: [
         {name: 'farmExperience', type: 'mult', value: 1.3}
